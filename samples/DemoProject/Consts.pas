aa3ca8ed9a8ec666b887267799e95e1cee5549c6
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-10-24 10:35:47 +0200

    added header copyright comments

--- samples/DemoProject/Consts.pas
+++ samples/DemoProject/Consts.pas
@@ -1,3 +1,40 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// CliTseg API client for Delphi
+///
+/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Client units and samples for the Ciltseg API to manage and use licenses
+/// keys in your Delphi project.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://ciltseg.olfsoftware.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/CilTseg4Delphi
+///
+/// ***************************************************************************
+/// File last update : 2024-10-23T18:03:30.000+02:00
+/// Signature : eb690618ecbd7ef1cb7d80b573e3de4b83024803
+/// ***************************************************************************
+/// </summary>
+
 unit Consts;
 
 interface
--- samples/DemoProject/DemoProject.dpr
+++ samples/DemoProject/DemoProject.dpr
@@ -1,3 +1,40 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// CliTseg API client for Delphi
+///
+/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Client units and samples for the Ciltseg API to manage and use licenses
+/// keys in your Delphi project.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://ciltseg.olfsoftware.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/CilTseg4Delphi
+///
+/// ***************************************************************************
+/// File last update : 2024-10-23T19:27:34.000+02:00
+/// Signature : d50160f59411d22886493903947a1d9abc43246d
+/// ***************************************************************************
+/// </summary>
+
 program DemoProject;
 
 uses
