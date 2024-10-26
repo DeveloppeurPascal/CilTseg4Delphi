a52ef38697251817965b04cdc9158252b9c1303e
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-10-26 21:17:29 +0200

    added a VCL "real case" sample project

---
+++ samples/CommercialProject/uConfig.pas
@@ -0,0 +1,184 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// CliTseg API client for Delphi
+///
+/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Client units and samples for the Ciltseg API to manage and use licenses
+/// keys in your Delphi project.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://ciltseg.olfsoftware.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/CilTseg4Delphi
+///
+/// ***************************************************************************
+/// File last update : 2024-10-26T21:13:24.000+02:00
+/// Signature : 6dba1f157ad5d7922719707d436570198e4b9636
+/// ***************************************************************************
+/// </summary>
+
+unit uConfig;
+
+interface
+
+type
+  TConfig = class
+  private
+    FLicenseEmail: string;
+    FLicenseDeviceName: string;
+    FLicenseActivationNumber: string;
+    FLicenseNumber: string;
+    procedure SetLicenseActivationNumber(const Value: string);
+    procedure SetLicenseDeviceName(const Value: string);
+    procedure SetLicenseEmail(const Value: string);
+    procedure SetLicenseNumber(const Value: string);
+  protected
+  public
+    /// <summary>
+    /// License number, given by the user with its email address when registering the software
+    /// </summary>
+    property LicenseNumber: string read FLicenseNumber write SetLicenseNumber;
+    /// <summary>
+    /// User email, given by the user with its license number when registering the software
+    /// </summary>
+    property LicenseEmail: string read FLicenseEmail write SetLicenseEmail;
+    /// <summary>
+    /// Activatio number, given by the server if the license has been activated
+    /// </summary>
+    property LicenseActivationNumber: string read FLicenseActivationNumber
+      write SetLicenseActivationNumber;
+    /// <summary>
+    /// Not supposed to be stored, a unique (or not) name for this device, to associate with this license activation and check sometimes to verify the license has not been copied on an other computer
+    /// (you can use the device name, the ID of the computer or its hard drive, ...)
+    /// </summary>
+    property LicenseDeviceName: string read FLicenseDeviceName
+      write SetLicenseDeviceName;
+    /// <summary>
+    /// Returns the singleton instance of this configuration
+    /// </summary>
+    class function Current: TConfig;
+    constructor Create;
+    procedure Load;
+    procedure Save;
+  end;
+
+implementation
+
+uses
+  System.IOUtils,
+  System.IniFiles,
+  uGetDeviceName;
+
+var
+  Config: TConfig;
+
+  { TConfig }
+
+constructor TConfig.Create;
+begin
+  inherited;
+  FLicenseEmail := '';
+  FLicenseDeviceName := getDeviceName;
+  FLicenseActivationNumber := '';
+  FLicenseNumber := '';
+end;
+
+class function TConfig.Current: TConfig;
+begin
+  if not assigned(Config) then
+  begin
+    Config := TConfig.Create;
+    Config.Load;
+  end;
+  result := Config;
+end;
+
+procedure TConfig.Load;
+var
+  ini: TIniFile;
+  section: string;
+begin
+  // use a more secured way to store licenses data
+  if tfile.Exists(tpath.Combine(tpath.GetDocumentsPath, 'CilTsegSamples.lic'))
+  then
+  begin
+    ini := TIniFile.Create(tpath.Combine(tpath.GetDocumentsPath,
+      'CilTsegSamples.lic'));
+    try
+      section := '[' + tpath.GetFileNameWithoutExtension(paramstr(0)) + ']';
+      FLicenseEmail := ini.ReadString(section, 'le', '');
+      FLicenseDeviceName := ini.ReadString(section, 'ldn', getDeviceName);
+      FLicenseActivationNumber := ini.ReadString(section, 'lan', '');
+      FLicenseNumber := ini.ReadString(section, 'ln', '');
+    finally
+      ini.free;
+    end;
+  end;
+end;
+
+procedure TConfig.Save;
+var
+  ini: TIniFile;
+  section: string;
+begin
+  // use a more secured way to store licenses data
+  ini := TIniFile.Create(tpath.Combine(tpath.GetDocumentsPath,
+    'CilTsegSamples.lic'));
+  try
+    section := '[' + tpath.GetFileNameWithoutExtension(paramstr(0)) + ']';
+    ini.WriteString(section, 'le', FLicenseEmail);
+    ini.WriteString(section, 'ldn', FLicenseDeviceName);
+    ini.WriteString(section, 'lan', FLicenseActivationNumber);
+    ini.WriteString(section, 'ln', FLicenseNumber);
+  finally
+    ini.free;
+  end;
+end;
+
+procedure TConfig.SetLicenseActivationNumber(const Value: string);
+begin
+  FLicenseActivationNumber := Value;
+end;
+
+procedure TConfig.SetLicenseDeviceName(const Value: string);
+begin
+  FLicenseDeviceName := Value;
+end;
+
+procedure TConfig.SetLicenseEmail(const Value: string);
+begin
+  FLicenseEmail := Value;
+end;
+
+procedure TConfig.SetLicenseNumber(const Value: string);
+begin
+  FLicenseNumber := Value;
+end;
+
+initialization
+
+Config := nil;
+
+finalization
+
+Config.free;
+
+end.
---
+++ samples/CommercialProject/uConsts.pas
@@ -0,0 +1,117 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// CliTseg API client for Delphi
+///
+/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Client units and samples for the Ciltseg API to manage and use licenses
+/// keys in your Delphi project.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://ciltseg.olfsoftware.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/CilTseg4Delphi
+///
+/// ***************************************************************************
+/// File last update : 2024-10-26T20:56:18.000+02:00
+/// Signature : e1b5bbc954c060f05eab772d3d41cc39fb5b3177
+/// ***************************************************************************
+/// </summary>
+
+unit uConsts;
+
+interface
+
+const
+  /// <summary>
+  /// Date of this current release as a string in ISO format (YYYY-MM-DD)
+  /// </summary>
+  /// <remarks>
+  /// Needed to use CilTseg API,
+  /// don't share it in a public code repository,
+  /// store it as a const or string variable in your source code.
+  /// </remarks>
+  CVersionDate = '2024-10-20';
+
+  /// <summary>
+  /// Version of this current release (not used by the API, it's a string, use what you want in it)
+  /// </summary>
+  CVersionNumber = '1.0.0';
+
+  /// <summary>
+  /// Your selling web page URL for this software
+  /// </summary>
+  /// <remarks>
+  /// It's better to use a redirection URL if you want to change your selling
+  /// URL without compiling and distribution a new binary, but you can if the
+  /// address is supposed to be stable.
+  /// </remarks>
+  CSoftwareBuyURL = 'https://localhost/BuyThisSoftware';
+
+  /// <summary>
+  /// Name of the platform for this program, depending on its operating
+  /// system, compiler or other things. It should correspond to a platform
+  /// you declared in CilTseg and use as a file attached to new releases of
+  /// the software.
+  /// </summary>
+{$IF Defined(MSWINDOWS)}
+  CSoftwareCurrentPlatform = 'Windows';
+{$ELSE IF Defined(MACOS)}
+  CSoftwareCurrentPlatform = 'macOS';
+{$ENDIF}
+  /// <summary>
+  /// ID given by the CilTseg backoffice for this software
+  /// </summary>
+  /// <remarks>
+  /// Needed to use CilTseg API,
+  /// don't share it in a public code repository,
+  /// store it as a const or integer variable in your source code.
+  /// </remarks>
+  CCiltsegSoftwareID = 4;
+
+  /// <summary>
+  /// Private token given by the CilTseg backoffice for this software
+  /// </summary>
+  /// <remarks>
+  /// Needed to use CilTseg API,
+  /// don't share it in a public code repository,
+  /// store it as a const or string variable in your source code.
+  /// Don't store it in your screen (DFM or FMX files), it will be in the EXE
+  /// resources and could be readable as it.
+  /// If you can, crypt it in your code and uncrypt it when you need to use it.
+  /// </remarks>
+  CCiltsegSoftwareToken =
+    'LX7r0rCMeDOLlaAezqvCbW9phKxOq79usqqH1Byhq8yOukaqE9gAEr56r10';
+
+  /// <summary>
+  /// CilTseg API server URL
+  /// </summary>
+  /// <remarks>
+  /// Needed to use CilTseg API,
+  /// don't share it in a public code repository,
+  /// store it as a const or string variable in your source code.
+  /// Don't store it in your screen (DFM or FMX files), it will be in the EXE
+  /// resources and could be readable as it.
+  /// </remarks>
+  CCiltsegServerURL = 'http://localhost/ciltseg-server/src/';
+
+implementation
+
+end.
